7827740490c81f2a9e191d10215e3e1cd3f88a5c
GOKULAPRIYAidayakottai <30829571+GOKULAPRIYAidayakottai@users.noreply.github.com>
2018-03-11 13:54:37 +0530

    Update longest_common_prefix.c

--- longest_common_prefix.c
+++ longest_common_prefix.c
@@ -1,17 +1,18 @@
 
 #include <stdio.h>
-
+#include<string.h>
 int main()
 {
     char s[5][5],c;
-    int n,j,i,t=1,temp=0;
+    int n,j,i,t=1,temp=0,l=0;
     printf("enter no of strings");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%s",&s[i]);
     }
-    for(i=0;i<n;i++)
+    l=strlen(s[0]);
+    for(i=0;i<l;i++)
     {
         c=s[0][i];
         for(j=1;j<n;j++)
